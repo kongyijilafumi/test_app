2e13b8c7d0277e933d1dc331f2472344f228ea58
孔乙己拉夫米 <1369501150@qq.com>
2021-08-01 08:23:06 +0800

    change file

---
+++ src/page/index.js
@@ -0,0 +1,9 @@
+import "./index.less"
+import { Button } from "antd"
+
+export default function Index() {
+  return <div className="index-container">
+    <h1>there is index page</h1>
+    <Button type="primary">index button</Button>
+  </div>
+}
