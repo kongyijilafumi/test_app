826550734767050a859d8f8434e9e3758105c1d1
孔乙己拉夫米 <zhangzhengzeng131@qq.com>
2022-04-14 16:49:59 +0800

    init

--- src/page/index.js
+++
@@ -1,9 +0,0 @@
-import "./index.less"
-import { Button } from "antd"
-
-export default function Index() {
-  return <div className="index-container">
-    <h1>there is index page</h1>
-    <Button type="primary">index button</Button>
-  </div>
-}
