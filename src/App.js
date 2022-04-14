4baf8f982b05fc3ab8359a49dbacf2c0c3f1de93
孔乙己拉夫米 <zhangzhengzeng131@qq.com>
2022-04-14 18:30:02 +0800

    test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Index from "./page/index"
 function App() {
   return (
     <div className="App">
-      react app -test1
+      react app -test12
       <hr></hr>
       <Index />
     </div>
